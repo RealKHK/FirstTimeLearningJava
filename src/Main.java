3c96bbe8f571d772b456a934dc5657d74890bee3
RealKHK <myothant18.1996@gmail.com>
2023-09-08 06:25:32 +0630

    HAHAHAHHA

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); /* wth */
+        int z = 30;
+        int y = 10;
+        String x = "Welcome to minecraft";
+        int i = y + z;
+        System.out.println(x + " " + i);
+        String k = x + " " + "bruh";
+        System.out.println(k);
+        }
+
+
+    }
+
